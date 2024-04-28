8bda2047459327734c0efd9197a21d6a9fc1e33a
Gagan pallai <112757838+IamGagan444@users.noreply.github.com>
2024-04-28 12:23:25 +0530

    added gagagn

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express= require("express")
 const app=express()
 require('dotenv').config()
 const port=process.env.PORT
- 
+ const gagan={
+    name:"gagan pallai",
+    village:"kalajamuna"
+    ,pasion:"developer"
+    ,hoby:"confused"
+ }
 app.get("/",(req,res)=>{
     return res.send("how are you ")
+})
+app.get("/gagan",(req,res)=>{
+
+  res.json(gagan)
+
 })
 app.get("/login",(req,res)=>{
     return res.send("<h1>this is the login page <h1/>")
